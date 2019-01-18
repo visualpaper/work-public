03ef642c7521f3d55b4347447b122cdfe0e90e66
visualpaper <visuallaper@yahoo.co.jp>
2019-01-19 01:16:44 +0900

    modify

--- git-study/BinatyAAAHeader.java
+++ git-study/BinatyAAAHeader.java
@@ -1,21 +1,4 @@
 
-public interface ResponseHeader222222222333333333333333 {
-
-  @Nonnull
-  ResponseBuilder applyTo(@Nonnull ResponseBuilder builder);
-
-  @Nonnull
-  default ResponseModifier convert() {
-    return builder -> header.applyTo(builder).build();
-  }
-
-  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-
-
-  }
-}
-}
-   }
+public interface 3333333333333 {
+  aaaaaaaaaaa
 }
