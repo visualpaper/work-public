e7ea26887ae0fbfe8c0ce2e98526b8151a84911e
visualpaper <visuallaper@yahoo.co.jp>
2019-05-25 00:55:38 +0900

    まずは単一プロジェクトでできるように。次は build の中に pluginManagement を持つ

---
+++ maven-study/other/src/main/java/com/example/visualp/system190524/facade/impl/SampleConst.groovy
@@ -0,0 +1,6 @@
+package com.example.visualp.system190524.facade.impl
+
+class SampleConst {
+    public static VALUE = "aa";
+
+}
---
+++ maven-study/other/src/main/resources/sample.groovy
@@ -0,0 +1,4 @@
+import com.example.visualp.system190524.facade.impl.SampleConst
+
+println SampleConst.VALUE
+println 1 + 1
