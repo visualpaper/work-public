9af32531abb055356a03e8e596373dd42309efdf
visualpaper <visuallaper@yahoo.co.jp>
2019-06-02 00:58:06 +0900

    cron 受付まで実装

--- scheduler-application/jersey/src/main/java/com/example/visualp/system19053101/facade/impl/ScheduleFacadeImpl.java
+++ scheduler-application/jersey/src/main/java/com/example/visualp/system19053101/facade/impl/ScheduleFacadeImpl.java
@@ -1,12 +1,23 @@
 package com.example.visualp.system19053101.facade.impl;
 
 import com.example.visualp.system19053101.facade.ScheduleFacade;
+import java.text.ParseException;
+import java.util.TimeZone;
 import javax.annotation.Nonnull;
+import org.quartz.CronExpression;
 
 public class ScheduleFacadeImpl implements ScheduleFacade {
 
   @Override
   public void add(@Nonnull String cronTab) {
-    System.out.println(cronTab);
+    CronExpression expression;
+
+    try {
+      // 秒未サポートなら、先頭に "* " を追加すれば OK
+      expression = new CronExpression(cronTab);
+    } catch (ParseException e) {
+      throw new IllegalArgumentException(e);
+    }
+    // この時点で cron tab 登録可能なものなので dynamodb に登録して OK
   }
 }
