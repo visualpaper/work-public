0a5e314f8262d7477d78dc1b617728846100f144
visualpaper <visuallaper@yahoo.co.jp>
2019-06-02 02:44:29 +0900

    base64 streaming 調査/実装まで

--- s3/src/main/java/com/example/visualp/system010/facade/impl/StudyFacadeImpl.java
+++ s3/src/main/java/com/example/visualp/system010/facade/impl/StudyFacadeImpl.java
@@ -2,7 +2,10 @@ package com.example.visualp.system010.facade.impl;
 
 import com.example.visualp.system010.accessor.s3.S3Accessor;
 import com.example.visualp.system010.facade.StudyFacade;
+import java.io.InputStream;
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import org.apache.commons.codec.binary.Base64InputStream;
 
 public class StudyFacadeImpl implements StudyFacade {
 
@@ -14,5 +17,19 @@ public class StudyFacadeImpl implements StudyFacade {
     String location = accessor.getLocation();
 
     System.out.println(location);
+  }
+
+  @Nonnull
+  @Override
+  public InputStream getBase64Binary() throws Exception {
+    return accessor.getBase64Binary();
+  }
+
+  @Override
+  public void putBase64Binary(@Nonnull InputStream is, long contentLength) {
+    accessor.putBase64Binary(
+        new Base64InputStream(is, false),
+        contentLength
+    );
   }
 }
--- s3/src/main/java/com/example/visualp/system010/resources/StudyResources.java
+++ s3/src/main/java/com/example/visualp/system010/resources/StudyResources.java
@@ -2,14 +2,21 @@ package com.example.visualp.system010.resources;
 
 import com.example.visualp.system010.facade.StudyFacade;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("study")
 public class StudyResources {
+
+  @Context
+  private ContainerRequestContext context;
 
   @Inject
   private StudyFacade facade;
@@ -17,8 +24,32 @@ public class StudyResources {
   @Path("location")
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public Response s3Get() throws Exception {
+  public Response location() throws Exception {
     facade.getLocation();
+
+    return Response
+        .ok()
+        .build();
+  }
+
+  @Path("base64Binary")
+  @GET
+  @Produces(MediaType.APPLICATION_OCTET_STREAM)
+  public Response getBase64Binary() throws Exception {
+    return Response
+        .ok(facade.getBase64Binary())
+        .build();
+  }
+
+  @Path("base64Binary")
+  @PUT
+  @Consumes(MediaType.APPLICATION_OCTET_STREAM)
+  public Response putBase64Binary() throws Exception {
+
+    facade.putBase64Binary(
+        context.getEntityStream(),
+        context.getLength()
+    );
 
     return Response
         .ok()
